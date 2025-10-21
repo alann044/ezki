6f46e400f94cd6d8de26eb25769997273382860b
alann044 <adriankfc397@gmail.com>
2025-10-20 20:51:57 -0600

    Add files via upload

--- ezkistudiosdb.sql
+++ ezkistudiosdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3307
--- Tiempo de generación: 20-10-2025 a las 19:55:24
+-- Tiempo de generación: 21-10-2025 a las 04:48:20
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -24,11 +24,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `IdCliente` int(11) NOT NULL,
+  `Nombre` varchar(100) NOT NULL,
+  `Empresa` varchar(100) DEFAULT NULL,
+  `Correo` varchar(100) DEFAULT NULL,
+  `Telefono` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `mensajescontacto`
 --
 
 CREATE TABLE `mensajescontacto` (
   `IdMensaje` int(11) NOT NULL,
+  `IdCliente` int(11) DEFAULT NULL,
   `Nombre` varchar(100) NOT NULL,
   `Correo` varchar(100) NOT NULL,
   `Asunto` varchar(100) DEFAULT NULL,
@@ -40,8 +55,8 @@ CREATE TABLE `mensajescontacto` (
 -- Volcado de datos para la tabla `mensajescontacto`
 --
 
-INSERT INTO `mensajescontacto` (`IdMensaje`, `Nombre`, `Correo`, `Asunto`, `Mensaje`, `FechaEnvio`) VALUES
-(1, 'Alan', 'alan@gmail.com', 'Hola cómo estás', 'Hola como estás yo muy bien y decidido a romper esquemas', '2025-10-20 11:29:18');
+INSERT INTO `mensajescontacto` (`IdMensaje`, `IdCliente`, `Nombre`, `Correo`, `Asunto`, `Mensaje`, `FechaEnvio`) VALUES
+(1, NULL, 'Alan', 'alan@gmail.com', 'Hola cómo estás', 'Hola como estás yo muy bien y decidido a romper esquemas', '2025-10-20 11:29:18');
 
 -- --------------------------------------------------------
 
@@ -51,6 +66,7 @@ INSERT INTO `mensajescontacto` (`IdMensaje`, `Nombre`, `Correo`, `Asunto`, `Mens
 
 CREATE TABLE `testimonios` (
   `IdTestimonio` int(11) NOT NULL,
+  `IdCliente` int(11) DEFAULT NULL,
   `NombreCliente` varchar(100) NOT NULL,
   `Empresa` varchar(100) DEFAULT NULL,
   `Comentario` text NOT NULL,
@@ -62,28 +78,43 @@ CREATE TABLE `testimonios` (
 -- Volcado de datos para la tabla `testimonios`
 --
 
-INSERT INTO `testimonios` (`IdTestimonio`, `NombreCliente`, `Empresa`, `Comentario`, `Fecha`, `ImagenCliente`) VALUES
-(1, 'Alan Esquivel', 'EzkiStudios', 'Muy bonito todo', '2025-10-20', '');
+INSERT INTO `testimonios` (`IdTestimonio`, `IdCliente`, `NombreCliente`, `Empresa`, `Comentario`, `Fecha`, `ImagenCliente`) VALUES
+(1, NULL, 'Alan Esquivel', 'EzkiStudios', 'Muy bonito todo', '2025-10-20', '');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`IdCliente`),
+  ADD UNIQUE KEY `Correo` (`Correo`);
+
+--
 -- Indices de la tabla `mensajescontacto`
 --
 ALTER TABLE `mensajescontacto`
-  ADD PRIMARY KEY (`IdMensaje`);
+  ADD PRIMARY KEY (`IdMensaje`),
+  ADD KEY `FK_Mensajes_Clientes` (`IdCliente`);
 
 --
 -- Indices de la tabla `testimonios`
 --
 ALTER TABLE `testimonios`
-  ADD PRIMARY KEY (`IdTestimonio`);
+  ADD PRIMARY KEY (`IdTestimonio`),
+  ADD KEY `FK_Testimonios_Clientes` (`IdCliente`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `IdCliente` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `mensajescontacto`
@@ -96,6 +127,22 @@ ALTER TABLE `mensajescontacto`
 --
 ALTER TABLE `testimonios`
   MODIFY `IdTestimonio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `mensajescontacto`
+--
+ALTER TABLE `mensajescontacto`
+  ADD CONSTRAINT `FK_Mensajes_Clientes` FOREIGN KEY (`IdCliente`) REFERENCES `clientes` (`IdCliente`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `testimonios`
+--
+ALTER TABLE `testimonios`
+  ADD CONSTRAINT `FK_Testimonios_Clientes` FOREIGN KEY (`IdCliente`) REFERENCES `clientes` (`IdCliente`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
